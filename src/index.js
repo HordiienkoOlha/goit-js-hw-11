999baf1f66fe61bd0b40f69f5b5dababc36cc1a8
HordiienkoOlha <olhahordiienkomail@gmail.com>
2022-04-15 23:29:48 +0300

    edit form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './sass/main.scss';
 // const bootstrap = require('bootstrap');
 // import bootstrap from 'bootstrap';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// Notify.success('Sol lucet omnibus');
+// Notify.failure('Qui timide rogat docet negare');
+// Notify.warning('Memento te hominem esse');
+// Notify.info('Cogito ergo sum');
