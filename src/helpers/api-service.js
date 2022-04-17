0c516f11befc1d2d7b2215540af3a971f665ff2d
HordiienkoOlha <olhahordiienkomail@gmail.com>
2022-04-17 18:05:11 +0300

    add get axios

--- src/helpers/api-service.js
+++ src/helpers/api-service.js
@@ -1,9 +1,21 @@
 import axios from 'axios';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const API_KEY = '26797371-301fbf489e1e7afb05e25635e';
 const BASE_URL = 'https://pixabay.com/api';
 axios.defaults.baseURL = BASE_URL;
-axios.defaults.headers.common['Authorization'] = API_KEY;
+
+export const searchPhoto = () => {
+    return axios
+      .get(
+          `/?key=${API_KEY}=cat&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`,
+        
+      )
+}
+// default { searchPhoto };
+
+// console.log(API_KEY)
+
 // export const ImageService = {
 //   _query: '',
 //   page: 1,
@@ -32,16 +44,5 @@ axios.defaults.headers.common['Authorization'] = API_KEY;
 //     this._query = newQuery;
 //   },
 // };
-// function fetchCountries(name) {
-    
 
-//   return fetch('https://pixabay.com/api/?key=26797371-301fbf489e1e7afb05e25635e&q=${}&image_type=photo&orientation=horizontal&safesearch=true').then(r => {
-//     if (!r.ok) {
-//       Notify.failure('Oops, there is no country with that name');
-//       throw Error(`Error: ${r.statusText}`);
-//       }
-//       console.log(r)
-//     return r.json();
-//   });
-// }
-// fetchCountries()
+
--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import './sass/main.scss';
 import 'material-icons/iconfont/material-icons.css';
-
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import * as PhotoService from './helpers/api-service';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // Notify.success('Sol lucet omnibus');
 // Notify.failure('Qui timide rogat docet negare');
 // Notify.warning('Sorry, there are no images matching your search query. Please try again.');
 // Notify.info('We're sorry, but you've reached the end of search results.');
 //
-// key - твій унікальний ключ доступу до API.
-// q - термін для пошуку. Те, що буде вводити користувач.
-// image_type - тип зображення. На потрібні тільки фотографії, тому постав значення photo.
-// orientation - орієнтація фотографії. Постав значення horizontal.
-// safesearch - фільтр за віком. Постав значення true.
-
+console.log(PhotoService)
 import { getRefs } from './helpers/getRefs';
 import { ImageService } from './helpers/api-service';
 const { formRef, inputRef, btnSearchRef, btnLoadMoreRef } = getRefs();
@@ -25,9 +20,10 @@ function onSearchSubmit(event) {
     const query= event.currentTarget.elements.query.value.trim();
     console.dir(query)
 }
-// const getContacts = async () => {
-//   const { data } = await axios.get('/images');
-//     console.log(data)
-//   return data;
-// };
-// axios.get(url[, config])
+PhotoService.searchPhoto().then(responce => {
+    Notify.success('Find photo');
+    console.log(responce)
+}).catch(err => {
+    Notify.failure('Error');
+    console.error(err.message)
+})
